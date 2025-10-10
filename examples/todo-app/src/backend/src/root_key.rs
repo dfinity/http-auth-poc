1d79cd1524a104144b5cdf92f5cdacbb83be3bcc
ilbertt <luca.bertelli@dfinity.org>
2025-10-10 19:12:36 +0200

    fix: PR feedback

--- examples/todo-app/src/backend/src/root_key.rs
+++ examples/todo-app/src/backend/src/root_key.rs
@@ -4,14 +4,10 @@ use ic_cdk::api;
 
 static ROOT_KEY: OnceLock<Vec<u8>> = OnceLock::new();
 
-pub fn with_root_key<F, R>(f: F) -> R
-where
-    F: FnOnce(&[u8]) -> R,
-{
+pub fn get_root_key() -> &'static [u8] {
     // Unwrap is safe because the root key is initialized in init_root_key
     // during canister initialization and upgrade
-    let root_key = ROOT_KEY.get().unwrap();
-    f(root_key)
+    ROOT_KEY.get().unwrap()
 }
 
 pub fn init_root_key() {
