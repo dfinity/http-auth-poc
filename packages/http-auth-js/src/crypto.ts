a44fbceb77473937aa6867735526fc7bca3c2671
ilbertt <luca.bertelli@dfinity.org>
2025-10-10 11:59:09 +0200

    build: update js dependencies
    Additionally, updates the vite config to also check for typescript types

--- packages/http-auth-js/src/crypto.ts
+++ packages/http-auth-js/src/crypto.ts
@@ -7,17 +7,15 @@ export function generateNonce(): string {
 }
 
 export async function sha256(
-  data: string | ArrayBuffer | ArrayBufferView,
+  data: string | BufferSource,
 ): Promise<ArrayBuffer> {
   if (typeof data === 'string') {
-    return await sha256Buffer(new TextEncoder().encode(data));
+    data = new TextEncoder().encode(data);
   }
 
   return await sha256Buffer(data);
 }
 
-async function sha256Buffer(
-  data: ArrayBuffer | ArrayBufferView,
-): Promise<ArrayBuffer> {
+async function sha256Buffer(data: BufferSource): Promise<ArrayBuffer> {
   return await crypto.subtle.digest('SHA-256', data);
 }
