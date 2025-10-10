d0d839d43ea8d6c455b6576ac7f81a23bd57222b
ilbertt <luca.bertelli@dfinity.org>
2025-10-10 19:06:45 +0200

    fix: PR feedback

--- examples/todo-app/src/backend/src/root_key.rs
+++ examples/todo-app/src/backend/src/root_key.rs
@@ -1,26 +1,20 @@
-use std::cell::RefCell;
+use std::sync::OnceLock;
 
 use ic_cdk::api;
 
-thread_local! {
-    static ROOT_KEY: RefCell<Option<Vec<u8>>> = const { RefCell::new(None) };
-}
+static ROOT_KEY: OnceLock<Vec<u8>> = OnceLock::new();
 
 pub fn with_root_key<F, R>(f: F) -> R
 where
     F: FnOnce(&[u8]) -> R,
 {
-    ROOT_KEY.with_borrow(|rk| {
-        // Unwrap is safe because the root key is initialized in init_root_key
-        // during canister initialization and upgrade
-        let root_key = rk.as_ref().unwrap();
-        f(root_key)
-    })
+    // Unwrap is safe because the root key is initialized in init_root_key
+    // during canister initialization and upgrade
+    let root_key = ROOT_KEY.get().unwrap();
+    f(root_key)
 }
 
 pub fn init_root_key() {
     let root_key = api::root_key();
-    ROOT_KEY.with_borrow_mut(|rk| {
-        rk.replace(root_key);
-    })
+    ROOT_KEY.set(root_key).unwrap();
 }
